f1504d2be06f53335fcfc5e1e951157a4c7ffb74
Fabian Fuchs <git@ffuchs.dev>
2026-08-06 20:20:06 +0200

    feat: make ui ready for release

--- packages/ui/src/routes/(test)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '../../lib/theme/flowbite.css';
-
-	const { children } = $props();
-</script>
-
-<main class="p-8">
-	{@render children()}
-</main>
--- packages/ui/src/routes/demo/login/+page.svelte
+++ packages/ui/src/routes/demo/login/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-	import {
-		Button,
-		Form,
-		Heading,
-		Link,
-		Strong,
-		Text,
-		Input,
-		Checkbox
-	} from '../../../lib/index.ts';
+	import { Button, Form, Heading, Link, Strong, Text, Input } from '../../../lib/index.ts';
 	import { superForm } from 'sveltekit-superforms';
 	import { zod4 } from 'sveltekit-superforms/adapters';
 	import { untrack } from 'svelte';
@@ -43,7 +34,6 @@
 	<div class="flex items-center justify-between">
 		<Form.Field auto name="remember">
 			<Form.Control>
-				<Checkbox />
 				<Form.Label>Remember me</Form.Label>
 			</Form.Control>
 			<Form.Error />
