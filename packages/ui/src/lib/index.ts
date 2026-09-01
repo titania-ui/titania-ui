3ab8a01afb97199640109c24face47281cc25479
Fabian Fuchs <git@ffuchs.dev>
2026-09-01 20:53:10 +0200

    chore: sort exports

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,36 +1,26 @@
-// Buttons
+// Components
+export { default as Alert } from './components/alert/index.js';
 export { default as Button } from './components/button/index.js';
+export { default as Chip } from './components/chip/index.js';
 export { default as CloseButton } from './components/close-button/index.js';
-
-// Forms
-export { default as Form } from './components/form/index.js';
+export { default as DescriptionList } from './components/description-list/index.js';
 export { default as Field } from './components/field/index.js';
-export { default as Label } from './components/label/index.js';
+export { default as Form } from './components/form/index.js';
 export { default as Input } from './components/input/index.js';
 export { default as InputGroup } from './components/inputgroup/index.js';
-export { default as Textarea } from './components/textarea/index.js';
+export { default as Label } from './components/label/index.js';
 export { default as Select } from './components/select/index.js';
-
-// Feedback
-export { default as Alert } from './components/alert/index.js';
+export { default as Sidebar } from './components/sidebar/index.js';
 export { default as Spinner } from './components/spinner/index.js';
-
-// Display
-export { default as Chip } from './components/chip/index.js';
-export { default as DescriptionList } from './components/description-list/index.js';
-
-// Typography
+export { default as Textarea } from './components/textarea/index.js';
+export { default as Code } from './components/typography/code.js';
 export { default as Heading } from './components/typography/heading.js';
-export { default as Text } from './components/typography/text.js';
+export { default as Icon } from './components/typography/icon.js';
 export { default as Link } from './components/typography/link.js';
 export { default as Strong } from './components/typography/strong.js';
-export { default as Icon } from './components/typography/icon.js';
-export { default as Code } from './components/typography/code.js';
+export { default as Text } from './components/typography/text.js';
 
 // Helpers
-export { useHover } from './helpers/useHover.js';
 export { useActivePress } from './helpers/useActivePress.js';
 export { useFocusRing } from './helpers/useFocusRing.js';
-
-// Sidebar
-export { default as Sidebar } from './components/sidebar/index.js';
+export { useHover } from './helpers/useHover.js';
